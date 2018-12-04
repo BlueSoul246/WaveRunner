83fe1f2ff682a9b72a4f177dc8e47af7b41af67a
Brandon <BM848541@wcupa.edu>
2018-12-03 19:52:18 -0500

    Big Update
    **Forgot to push for a while**
    - Start and end screens are done
    - Levels are tweaked
    - Transitions between levels are coded

---
+++ Wave Runner/Assets/Scripts/CButtonManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CButtonManager : MonoBehaviour
+{
+    /*
+    public Button startGameButton;
+
+    public Camera cam;
+
+    private void Start()
+    {
+        startGameButton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //access the camera
+        //Camera cam = gameObject.GetComponent<Camera>();
+        cam = Camera.main;
+
+        //move cam to show controls
+        cam.transform.position = new Vector3(-1100, 1, -424.13f);
+
+        startGameButton.gameObject.SetActive(false);
+    }
+    */
+
+
+    public void StartGameButton(string newGame)
+    {
+        SceneManager.LoadScene(newGame);
+    }
+}
